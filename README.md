0cb4140db352d862219d8e8c066e8dd8eca8d378
Mr.Miss <keselekpermen@mrmiss.wtf>
2021-04-11 00:47:04 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bot Database Util
+
+Clean Gbanned user that banned with inappropriate or unnecessary word
+
+### words are compiled from : 
+
+- [Google Badword List](https://code.google.com/archive/p/badwordslist/downloads)
+
+- [onlyphantom/elangdev](https://raw.githubusercontent.com/onlyphantom/elangdev/master/elang/word2vec/utils/swear-words.txt)
+
---
+++ app.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const url = process.env.MONGO_URL;
+if(url == null){
+    console.log("No DB URL provided");
+    process.exit(1);
+}
+
+var BannedWords = fs.readFileSync("wordlist.txt").toString();
+BannedWords = BannedWords.replace(/\r\n/g,'\n').split('\n');
+
+
+console.log("Connecting to DB");
+
+MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+    if(err){
+        console.log("Unable to connect to DB\n", err);
+        process.exit(1);
+    } else {
+        console.log("connected");
+        const col = client.db("UbotIndo").collection("GBANS");
+
+        col.find({}).toArray((err, data) => {
+            if(err){
+                console.log(err);
+            } else {
+                console.log("cleaning banned user");
+                if(data.length){
+                    for(let user of data){
+                        for(let banned of BannedWords){
+                            if((user.reason).search(banned) != -1){  // delete if match
+                                col.deleteOne({_id: user._id});
+                                break;
+                            };
+                        }
+                    }
+                    console.log("done");
+                } else {
+                    console.log("No data on collection.");
+                }
+            }
+        })
+    }
+})
